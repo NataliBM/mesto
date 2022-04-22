b8b67029d1d370a98b757e88f37cb0a07e6137db
Natalia Buymova <nb@gs-inc.ru>
2022-04-22 18:40:59 +0300

    22.04_2 project4

--- scripts/index.js
+++ scripts/index.js
@@ -18,12 +18,15 @@ closeIcon.addEventListener('click', close);
 let formElement = document.querySelector('.form');
 let nameInput = document.querySelector('.form__input-name');
 let jobInput = document.querySelector('.form__input-about');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
 }
 formElement.addEventListener('submit', formSubmitHandler);
+
+let formButton = document.querySelector('.form__button');
+formButton.addEventListener('click', close);
